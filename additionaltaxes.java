6ff229efc4979a310f4b361a623b258f38de096e
yasaswinikunam <130962051+yasaswinikunam@users.noreply.github.com>
2023-07-09 23:13:57 +0530

    additionaltaxes.java

---
+++ additionaltaxes.java
@@ -0,0 +1,10 @@
+public class additionaltaxes {
+	private int rtocharges = 113990;
+	private int tcscharges = 11000;
+	public int getRtocharges() {
+		return rtocharges;
+	}
+	public int getTcscharges() {
+		return tcscharges;
+	}
+}
